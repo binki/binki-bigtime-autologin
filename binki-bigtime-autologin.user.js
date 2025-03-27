6127887a36ea48fa4856542190a53f319e9343ae
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2025-03-27 17:49:14 -0400

    Split out whenInputCompletedAsync().
    Fixes #1.

--- binki-bigtime-autologin.user.js
+++ binki-bigtime-autologin.user.js
@@ -7,6 +7,7 @@
 // @match https://intuit.bigtime.net/bigtime/*
 // @match https://intuit.bigtime.net/Bigtime
 // @match https://intuit.bigtime.net/Bigtime/*
+// @require https://github.com/binki/binki-userscript-when-input-completed/raw/d11bfc5021cb99fd80d5a2d008ffd4c7eabaf554/binki-userscript-when-input-completed.js
 // ==/UserScript==
 
 function requireSelector(selector, maybeContext) {
@@ -16,34 +17,16 @@ function requireSelector(selector, maybeContext) {
   throw new Error(`Unable to find selector ${selector}`);
 }
 
-function whenTextCompletedAsync(input, maybeAutoCompleteDelta) {
-  const inputEventName = 'input';
-  const autoCompleteDelta = maybeAutoCompleteDelta || 4;
-  return new Promise(resolve => {
-    let lastLength = 0;
-    const inputHandler = () => {
-      if (input.value.length >= autoCompleteDelta && Math.abs(input.value.length - lastLength) >= autoCompleteDelta) {
-        input.removeEventListener(inputEventName, inputHandler);
-        resolve();
-      } else {
-        lastLength = input.value.length;
-      }
-    };
-    input.addEventListener(inputEventName, inputHandler);
-    inputHandler();
-  });
-}
-
 (async () => {
   if (document.URL.startsWith('https://app.bigtime.net/')) {
     // This is really easy because these are prerendered pages.
-    await whenTextCompletedAsync(requireSelector('#Input_EmailAddress'));
+    await whenInputCompletedAsync(requireSelector('#Input_EmailAddress'));
     requireSelector('#rememberMe').setAttribute('checked', 'checked');
     requireSelector('button[name="Input.Button"]').click();
   } else if (document.URL.startsWith('https://intuit.bigtime.net/')) {
     // This is really easy because these are prerendered pages.
-    await whenTextCompletedAsync(requireSelector('#SUserName'));
-    await whenTextCompletedAsync(requireSelector('#SPassword'));
+    await whenInputCompletedAsync(requireSelector('#SUserName'));
+    await whenInputCompletedAsync(requireSelector('#SPassword'));
     requireSelector('#BRememberMe').setAttribute('checked', 'checked');
     requireSelector('#form_login button[type=submit], #form_login button:not([type])').click();
   }
